5da7b6ed2ba7ad6826a8778c696300d92cd5b95a
김태희 <clsrn7439@gmail.com>
2025-01-28 02:58:06 +0900

    기능 추가: deleteTimer 경로에 startTime 유효성 검사 추가
    - deleteTimer 유효성 검사를 확장하여 startTime 포함
    - 유효성 검사 메시지를 상수로 리팩토링

--- Back/src/validator/validator.ts
+++ Back/src/validator/validator.ts
@@ -1,30 +1,35 @@
 import { body, param } from "express-validator";
 
 const notEmptyMessage = (field: string) => `${field}을(를) 입력해야 합니다.`;
+const emailMessage = "유효한 이메일 주소를 입력해주세요.";
+const startTimeMessage = "시작 시간은 HH:MM 형식이어야 합니다.";
+const endTimeMessage = "종료 시간은 HH:MM 형식이어야 합니다.";
+const durationMessage = "지속 시간은 1 이상의 정수여야 합니다.";
+const dateMessage = "날짜는 YYYY-MM-DD 형식이어야 합니다.";
 
 export const timerValidators = {
   createTimer: [
     body("email")
       .isEmail()
-      .withMessage("유효한 이메일 주소를 입력해주세요.")
+      .withMessage(emailMessage)
       .notEmpty()
       .withMessage(notEmptyMessage("이메일 주소")),
 
     body("startTime")
       .matches(/^([01]\d|2[0-3]):([0-5]\d)$/)
-      .withMessage("시작 시간은 HH:MM 형식이어야 합니다.")
+      .withMessage(startTimeMessage)
       .notEmpty()
       .withMessage(notEmptyMessage("시작 시간")),
 
     body("endTime")
       .matches(/^([01]\d|2[0-3]):([0-5]\d)$/)
-      .withMessage("종료 시간은 HH:MM 형식이어야 합니다.")
+      .withMessage(endTimeMessage)
       .notEmpty()
       .withMessage(notEmptyMessage("종료 시간")),
 
     body("duration")
       .isInt({ min: 1 })
-      .withMessage("지속 시간은 1 이상의 정수여야 합니다.")
+      .withMessage(durationMessage)
       .notEmpty()
       .withMessage(notEmptyMessage("지속 시간")),
   ],
@@ -32,7 +37,7 @@ export const timerValidators = {
   getTimer: [
     param("email")
       .isEmail()
-      .withMessage("유효한 이메일 주소를 입력해주세요.")
+      .withMessage(emailMessage)
       .notEmpty()
       .withMessage(notEmptyMessage("이메일 주소")),
   ],
@@ -40,14 +45,20 @@ export const timerValidators = {
   deleteTimer: [
     param("email")
       .isEmail()
-      .withMessage("유효한 이메일 주소를 입력해주세요.")
+      .withMessage(emailMessage)
       .notEmpty()
       .withMessage(notEmptyMessage("이메일 주소")),
 
     param("date")
       .matches(/^\d{4}-\d{2}-\d{2}$/)
-      .withMessage("날짜는 YYYY-MM-DD 형식이어야 합니다.")
+      .withMessage(dateMessage)
       .notEmpty()
       .withMessage(notEmptyMessage("날짜")),
+
+    param("startTime")
+      .matches(/^([01]\d|2[0-3]):([0-5]\d)$/)
+      .withMessage(startTimeMessage)
+      .notEmpty()
+      .withMessage(notEmptyMessage("시작 시간")),
   ],
 };
