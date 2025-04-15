a03da4ffcbd2445dfec0fb505855d48f7ddf0f3d
김태희 <clsrn7439@gmail.com>
2025-04-15 16:58:52 +0900

    feat: 타이머 기록 추가 시 데이터 구조 변경
    - TimerClock 컴포넌트에서 타이머 기록 생성 시 기존 데이터에 새 기록을 추가하는 방식으로 변경

--- Front/components/timerClock/timerClock.tsx
+++ Front/components/timerClock/timerClock.tsx
@@ -53,7 +53,7 @@ export default function TimerClock() {
     onSuccess: (data, variables) => {
       // 타이머 기록이 추가되면 타이머 기록 목록 갱신
       if (user && user.email) {
-        queryClient.setQueryData(["timerRecords", user.email, selectDate], data);
+        queryClient.setQueryData(["timerRecords", user.email, selectDate], (old) => [...old, data]);
       } else {
         console.error("사용자 이메일이 없습니다.");
       }
